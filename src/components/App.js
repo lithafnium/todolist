36e292d176ebedb91adc671f18b0db499873e525
Steve Li <lithafnium2000@gmail.com>
2019-11-10 22:47:52 -0500

    fix header

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,17 @@ import logo from '../logo.svg';
 import './App.css';
 import Sidebar from './Sidebar'; 
 import Tasks from './Tasks'; 
-
+import Header from './Header'; 
 function App() {
   return (
-    <div class = "container">
+    <div>
+      <Header/>
+        <div class = "container">
+
         <Sidebar/>
         <Tasks/>
     
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react'; 
-
+import {FaCheckCircle}  from 'react-icons/fa';
 function Header(){
+
+	return(
+		<header>
+			<nav>
+				<div>
+          				<FaCheckCircle class = "logo"/>
+        		</div>
+			</nav>
+		</header>
+
+	); 
 
 }
 
-export default Sidebar; 
+export default Header; 
